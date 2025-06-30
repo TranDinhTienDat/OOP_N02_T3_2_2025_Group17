9527a1d529ecf98e64ea8a94efffb5be0aacb10c
duyneme <24100487@st.phenikaa-uni.edu.vn>
2025-06-30 09:49:47 +0000

    CAPNHAPMOI

--- quanLyPhongKham/src/App.java
+++ quanLyPhongKham/src/App.java
@@ -3,13 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-      //  testPatient tp = new  testPatient();
-
-      //  testPatient.test();
-      // tp.test();
-      BreakAndContinue.testBreakAndContinue();
-      int x = randNum.randomNumber();
-      System.out.println("Ramdom number: " + x);
+      testbook.meme();
 
 
     }
