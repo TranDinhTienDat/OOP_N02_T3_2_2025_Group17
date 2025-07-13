41cc875d83241116e6d34897f3f3befbaadae3f9
TranDinhTienDat <24100257@st.phenikaa-uni.edu.vn>
2025-07-13 21:22:38 +0700

    PhongKham.java

--- BenhVien/src/PhongKham.java
+++ BenhVien/src/PhongKham.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
 public class PhongKham {
-    private String tenPhongKham;
+    private String ten;
     private String diaChi;
     private ArrayList<BenhNhan> danhSachBenhNhan;
 
-    public PhongKham(String tenPhongKham, String diaChi) {
-        this.tenPhongKham = tenPhongKham;
+    public PhongKham(String ten, String diaChi) {
+        this.ten = ten;
         this.diaChi = diaChi;
         this.danhSachBenhNhan = new ArrayList<>();
     }
@@ -16,11 +16,21 @@ public class PhongKham {
     }
 
     public void hienThiBenhNhan() {
+        System.out.println("Danh sach benh nhan tai " + ten + ":");
         for (BenhNhan bn : danhSachBenhNhan) {
             System.out.println(bn);
         }
     }
+
+    public String getTen() { return ten; }
+    public String getDiaChi() { return diaChi; }
+
+    @Override
     public String toString() {
-        return "Tên phòng khám: " + tenPhongKham + ", Địa chỉ: " + diaChi;
+        return "PhongKham{" +
+                "ten='" + ten + '\'' +
+                ", diaChi='" + diaChi + '\'' +
+                ", soLuongBenhNhan=" + danhSachBenhNhan.size() +
+                '}';
     }
 }
