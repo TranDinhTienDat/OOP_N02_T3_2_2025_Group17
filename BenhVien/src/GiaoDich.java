3aafb5404466f67e8ddc3f10b8f76d6522373f8a
TranDinhTienDat <24100257@st.phenikaa-uni.edu.vn>
2025-07-13 21:20:48 +0700

    PhongKham.java

--- BenhVien/src/GiaoDich.java
+++ BenhVien/src/GiaoDich.java
@@ -1,35 +1,36 @@
-public class GiaoDich {
-    private String maGiaoDich;
-    private String maBenhNhan;
-    private double soTien;
-    private String ngay;
+import java.util.ArrayList;
 
-    public GiaoDich(String maGiaoDich, String maBenhNhan, double soTien, String ngay) {
-        this.maGiaoDich = maGiaoDich;
-        this.maBenhNhan = maBenhNhan;
-        this.soTien = soTien;
-        this.ngay = ngay;
+public class PhongKham {
+    private String ten;
+    private String diaChi;
+    private ArrayList<BenhNhan> danhSachBenhNhan;
+
+    public PhongKham(String ten, String diaChi) {
+        this.ten = ten;
+        this.diaChi = diaChi;
+        this.danhSachBenhNhan = new ArrayList<>();
     }
 
-    public String getMaGiaoDich() {
-        return maGiaoDich;
+    public void themBenhNhan(BenhNhan bn) {
+        danhSachBenhNhan.add(bn);
     }
 
-    public String getMaBenhNhan() {
-        return maBenhNhan;
+    public void hienThiBenhNhan() {
+        System.out.println("Danh sach benh nhan tai " + ten + ":");
+        for (BenhNhan bn : danhSachBenhNhan) {
+            System.out.println(bn);
+        }
     }
 
-    public double getSoTien() {
-        return soTien;
-    }
+    public String getTen() { return ten; }
+    public String getDiaChi() { return diaChi; }
 
-    public String getNgay() {
-        return ngay;
-    }
+    @Override
     public String toString() {
-        return "Mã giao dịch: " + maGiaoDich +
-               ", Mã bệnh nhân: " + maBenhNhan +
-               ", Số tiền: " + soTien +
-               ", Ngày: " + ngay;
+        return "PhongKham{" +
+                "ten='" + ten + '\'' +
+                ", diaChi='" + diaChi + '\'' +
+                ", soLuongBenhNhan=" + danhSachBenhNhan.size() +
+                '}';
     }
 }
