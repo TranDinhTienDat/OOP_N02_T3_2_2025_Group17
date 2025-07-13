f7db010ecd8339b0b9be5e0436a345c6cf529019
TranDinhTienDat <24100257@st.phenikaa-uni.edu.vn>
2025-07-13 21:25:55 +0700

    App.java

--- BenhVien/src/App.java
+++ BenhVien/src/App.java
@@ -1,15 +1,24 @@
 public class App {
     public static void main(String[] args) {
-        PhongKham phongKham = new PhongKham("Phòng Khám ABC", "123 Đường Lê Lợi");
+        // Tao phong kham
+        PhongKham phongKham = new PhongKham("Phong Kham ABC", "123 Duong Le Loi");
 
-        BenhNhan bn1 = new BenhNhan("BN001", "Nguyễn Văn A", 35, "Nam");
+        // Tao benh nhan
+        BenhNhan bn1 = new BenhNhan("BN001", "Nguyen Van A", 35, "Nam");
+        BenhNhan bn2 = new BenhNhan("BN002", "Tran Thi B", 29, "Nu");
+
+        // Them benh nhan vao phong kham
         phongKham.themBenhNhan(bn1);
+        phongKham.themBenhNhan(bn2);
 
+        // Tao giao dich
         GiaoDich gd1 = new GiaoDich("GD001", "BN001", 500000, "2025-07-13");
+        GiaoDich gd2 = new GiaoDich("GD002", "BN002", 300000, "2025-07-14");
 
+        // Hien thi thong tin
         System.out.println(phongKham);
         phongKham.hienThiBenhNhan();
         System.out.println(gd1);
+        System.out.println(gd2);
     }
 }
-
